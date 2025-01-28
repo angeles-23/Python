e4bf8b133bb30c29f6ecfc5bbcee7ba0276a6642
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-01-28 11:39:04 +0100

    Add files via upload

--- Tareas_python_2/ejercicio_17_contar_palabras.py
+++ Tareas_python_2/ejercicio_17_contar_palabras.py
@@ -1,4 +1,4 @@
-import os 
+import os, random
 os.system('cls')
 
 # Crea una función que reciba un texto (string) y devuelva las palabras únicas ordenadas alfabéticamente, ignorando mayúsculas/minúsculas, y cuántas veces aparece cada una.
@@ -15,12 +15,12 @@ os.system('cls')
 
 
 def mostrar_menu():
-    print('- '*57)
-    print('%-1s %60s %50s' %('|', 'CONTAR PALABRAS', '|'))
-    print('- '*57)
-    print('%1s %-105s %5s' %('|', '1. La cadena de texto es un input.', '|'))
-    print('%1s %-105s %5s' %('|', '2. la cadena de texto es una aleatoria de un Diccionario.', '|'))
-    print('- '*57)
+    print('- '*35)
+    print('%-1s %40s %26s' %('|', 'CONTAR PALABRAS', '|'))
+    print('- '*35)
+    print('%1s %-61s %5s' %('|', '1. La cadena de texto es un input.', '|'))
+    print('%1s %-61s %5s' %('|', '2. La cadena de texto es una aleatoria de un Diccionario.', '|'))
+    print('- '*35)
 
 def comprobar_opcion():
     while True:
@@ -50,20 +50,15 @@ def contar_palabras(cadena):
     return frecuencia_palabras
 
 
-
-# En caso de que sea una cadena introducida se pasa directamente a la funcion, y se introducira al diccionario
-# En caso de que sea un diccionario, entrar en los valores de cada uno de ellos
-# Separar las palabras por el caracter '.', contar las palabras
-
 if __name__ == '__main__':
     texto = {
-        1: "Hola mundo. ",
-        2: "El mundo es increíble." ,
-        3: "Hola de nuevo.", 
-        4: "Hoy es un dia increíble para programar. ",
-        5: "Este es mi primer hola mundo. ",
-        6: "Hoy voy a programar en Python.",
-        7: "Mi lenguaje de programación favorito es Python."
+        1: "Hola mundo y de nuevo hola mundo",
+        2: "El mundo es increíble" ,
+        3: "Hola de nuevo y adios de nuevo", 
+        4: "Hoy es un dia increíble para programar, el el dia del programador",
+        5: "Este es mi primer hola mundo",
+        6: "Hoy voy a programar en Python",
+        7: "Mi lenguaje de programación favorito es Python "
     }
 
     menu = mostrar_menu()
@@ -71,10 +66,21 @@ if __name__ == '__main__':
 
     match opcion:
         case 1:
-            texto = input('Introduce un texto: ')
+            texto = input('Introduce un texto: ').lower()
             cantidad_palabras = contar_palabras(texto)
             print(cantidad_palabras)
         case 2:
-            ...
-    
-    
+            textos_aleatorios = texto
+            numero_aleatorio = random.randint(1,6)
+
+            frases = str(textos_aleatorios[numero_aleatorio]).lower()
+            cantidad_palabras = contar_palabras(frases)
+            print(cantidad_palabras)
+
+            # Para todas las frases
+            # for textos in textos_aleatorios:
+            #     frases = str(textos_aleatorios[textos])
+            #     print(contar_palabras(frases))
+
+    print('\nLista ordenada')
+    print(sorted(cantidad_palabras))
