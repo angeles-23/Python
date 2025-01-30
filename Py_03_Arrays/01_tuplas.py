843e56ead7f211144126a4a4c71083e750f396d8
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-01-30 09:18:38 +0100

    Delete Py_03_Arrays directory

--- Py_03_Arrays/01_tuplas.py
+++
@@ -1,3 +0,0 @@
-# Tuplas: Son iniciadas y no se pueden modificar
-lenguajes = ('java', 'SQL', 'Python', 'HTML', 'XML', 'CSS', 'PHP')
-print(f'Vamos a aprender sobre el lenguaje de programación de {lenguajes[2]}')
--- Py_03_Arrays/03_listas.py
+++
@@ -1,8 +0,0 @@
-lenguajes = ['Java', 'SQL', 'Python', 'HTML', 'XML', 'CSS', 'PHP']
-
-print(f'Vamos a aprender sobre el lenguaje de programacion {lenguajes[2]}')
-
-
-# Añadir nuevos lenguajes
-lenguajes.append('JavaScript')
-print(lenguajes)
