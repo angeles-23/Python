6ad3879dd51700ad5b0d5612f58a5a348bec39df
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-01-30 09:19:07 +0100

    Delete Py_ejercicios_java directory

--- Py_ejercicios_java/Condicionales/01.py
+++
@@ -1,8 +0,0 @@
-numero = int(input('Introduce un numero: '))
-
-if(numero == 0):
-    print(f'El número {numero} es cero')
-elif(numero%2 == 0):
-    print(f'El número {numero} es par')
-else:
-    print(f'El número {numero} es impar')
