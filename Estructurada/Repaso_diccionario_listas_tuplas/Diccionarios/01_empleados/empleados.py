c592f786543671d793de8798ebcbfdd36f32def2
Your Name <you@example.com>
2025-03-05 22:27:21 +0100

    Actualizacion de diccionarios

--- Estructurada/Repaso_diccionario_listas_tuplas/Diccionarios/01_empleados/empleados.py
+++ Estructurada/Repaso_diccionario_listas_tuplas/Diccionarios/01_empleados/empleados.py
@@ -33,41 +33,6 @@ empleados = {
         "cargo": "Marketing",
         "salario": 50000,
         "fecha_ingreso": "2019-01-11"
-    },
-    1006: {
-        "nombre": "Javier Sánchez",
-        "edad": 45,
-        "cargo": "CTO",
-        "salario": 120000,
-        "fecha_ingreso": "2010-05-10"
-    },
-    1007: {
-        "nombre": "Raquel Fernández",
-        "edad": 38,
-        "cargo": "Product Manager",
-        "salario": 85000,
-        "fecha_ingreso": "2014-02-19"
-    },
-    1008: {
-        "nombre": "David Martín",
-        "edad": 29,
-        "cargo": "Desarrollador",
-        "salario": 52000,
-        "fecha_ingreso": "2020-11-30"
-    },
-    1009: {
-        "nombre": "Verónica García",
-        "edad": 25,
-        "cargo": "Diseñadora",
-        "salario": 48000,
-        "fecha_ingreso": "2022-02-25"
-    },
-    1010: {
-        "nombre": "Pedro Hernández",
-        "edad": 50,
-        "cargo": "Consultor",
-        "salario": 95000,
-        "fecha_ingreso": "2007-08-23"
     }
 }
 
