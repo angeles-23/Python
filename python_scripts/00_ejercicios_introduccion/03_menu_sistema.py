b990299c5ced8a6f17f5e6a88321262bd40cc756
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-03-25 12:49:17 +0100

    Add files via upload

--- python_scripts/00_ejercicios_introduccion/03_menu_sistema.py
+++ python_scripts/00_ejercicios_introduccion/03_menu_sistema.py
@@ -10,17 +10,18 @@ def main():
 
         match opcion:
             case 1:
-                ...
+                print(mostrar_fecha())
             case 2:
-                ...
+                mostar_usuario()
             case 3:
-                ...
+                mostrar_usuarios_conectados()
             case 4:
-                ...
+                mostrar_calendario_actual()
             case 5:
-                ...
+                cambiar_contrasenia()
             case 6:
-                ...
+                print('Saliendo...')
+                break
 
 
 def mostrar_menu():
@@ -47,14 +48,54 @@ def elegir_opcion():
 
 
 def mostrar_fecha():
-    ...
+    fecha_docker = subprocess.getoutput(['date'])
+    fecha = fecha_docker.split(' ')
+
+    dia_docker = fecha[0]
+    mes_docker = fecha[1]
+    anio_docker = fecha[5]
+
+    match mes_docker:
+        case 'Jan':
+            mes = 1
+        case 'Feb':
+            mes = 2
+        case 'Mar':
+            mes = 3
+        case 'Apr':
+            mes = 4
+        case 'May':
+            mes = 5
+        case 'Jun':
+            mes = 6
+        case 'Jul':
+            mes = 7
+        case 'Aug':
+            mes = 8
+        case 'Sep':
+            mes = 9
+        case 'Oct':
+            mes = 10
+        case 'Nov':
+            mes = 11
+        case 'Dec':
+            mes = 12
+
+    fecha_formateada = f'{dia_docker}/{mes}/{anio_docker}'
+    return fecha_formateada
+
 
 def mostar_usuario():
     ...
 
-def mostrar_usuarios_conrectados():
+def mostrar_usuarios_conectados():
     ...
 
+def mostrar_calendario_actual():
+    ...
+
+def cambiar_contrasenia():
+    ...
 
 
 
